0db12b9985a5799ad2cfb860f2770ab052ee76f3
Administrator <Administrator@scsbwin-919293.ITTPA.EBIZ.VERIZON.COM>
2016-09-27 09:14:02 -0700

    hackathon 2016

---
+++ Hackathon_2016/com/verizon/hackathon/web-content/login.php
@@ -0,0 +1,105 @@
+
+<?php
+session_start();
+// Do Something
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Login Page</title>
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/datepicker3.css" rel="stylesheet">
+<link href="css/styles.css" rel="stylesheet">
+
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<script src="js/respond.min.js"></script>
+<![endif]-->
+
+</head>
+
+<body>
+
+<?php 
+include '../shared/DbConnectionUtil.php';
+if(isset($_POST["username"])){
+	$userName = $_POST["username"];
+	$passWord = $_POST["password"];
+	
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$sql = "select id from user_login where username='$userName' and password='$passWord'";
+	
+	$result = $conn->query($sql);
+	
+	if ($result->num_rows > 0) {
+	$_SESSION["logged_user"] = $username;
+	header("location: index.php");
+	} else {
+		echo "<script>window.alert('Invalid User...')</script>";
+	}
+	
+	$conn->close();
+}
+?>
+	
+	<div class="row">
+		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
+			<div class="login-panel panel panel-default">
+				<div class="panel-heading">Log in</div>
+				<div class="panel-body">
+					<form role="form" method="POST">
+						<fieldset>
+							<div class="form-group">
+								<input class="form-control" placeholder="username" name="username" autofocus="">
+							</div>
+							<div class="form-group">
+								<input class="form-control" placeholder="Password" name="password" type="password" value="">
+							</div>
+							<div class="checkbox">
+								<label>
+									<input name="remember" type="checkbox" value="Remember Me">Remember Me
+								</label>
+							</div>
+							<input type="image" name="submit" class="btn btn-primary" alt="Login" />
+						</fieldset>
+					</form>
+				</div>
+			</div>
+		</div><!-- /.col-->
+	</div><!-- /.row -->	
+	
+		
+
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/chart.min.js"></script>
+	<script src="js/chart-data.js"></script>
+	<script src="js/easypiechart.js"></script>
+	<script src="js/easypiechart-data.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script>
+		!function ($) {
+			$(document).on("click","ul.nav li.parent > a > span.icon", function(){		  
+				$(this).find('em:first').toggleClass("glyphicon-minus");	  
+			}); 
+			$(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
+		}(window.jQuery);
+
+		$(window).on('resize', function () {
+		  if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+		})
+		$(window).on('resize', function () {
+		  if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+		})
+	</script>	
+</body>
+
+</html>
